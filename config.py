704ca7b55d39b6a257c076548a2148d997ff209b
Vimal Kumar <vimal78@gmail.com>
2026-03-27 20:28:07 +0530

    fixed config. increased dimention and seq length , and batch size

--- config.py
+++ config.py
@@ -4,13 +4,11 @@ from pathlib import Path
 def get_config():
     return {
         "datasource": "opus_books",
-        "batch_size": 8,
+        "batch_size": 32,
         "num_epochs": 20,
         "lr": 10**-4,
-        # "seq_len": 350,
-        "seq_len": 300,
-        # "d_model": 512,
-        "d_model": 256,
+        "seq_len": 350,
+        "d_model": 512,
         "lang_src": "en",
         "lang_tgt": "it",
         "model_folder": "weights",
