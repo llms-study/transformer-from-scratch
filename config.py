2ce98cd8f123faf8ca92a1ed7893d4e17f102104
Vimal Kumar <vimal78@gmail.com>
2026-03-27 20:31:41 +0530

    reduce batch size to 8

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 def get_config():
     return {
         "datasource": "opus_books",
-        "batch_size": 32,
+        "batch_size": 8,
         "num_epochs": 20,
         "lr": 10**-4,
         "seq_len": 350,
